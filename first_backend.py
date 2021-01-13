60548d59e113c14749053a7d4a4f20e8a1bf298e
Steven Ngo <stengo46@gmail.com>
2021-01-13 10:52:50 -0800

    Add ability to POST new users

--- first_backend.py
+++ first_backend.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -39,16 +40,26 @@ users = {
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST'])
 def get_users():
-    # Gets the value of any records matching 'name'
-    search_username = request.args.get('name')
-    if search_username:
-        subdict = {'users_list' : []}
-        for user in users['users_list']:
-            if user['name'] == search_username:
-                subdict['users_list'].append(user)
-        return subdict
+    if request.method == 'GET':
+        # Gets the value of any records matching 'name'
+        search_username = request.args.get('name')
+        if search_username:
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['name'] == search_username:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
+    
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+        # 200 = default code for a normal/OK response
+        resp.status_code = 200
+        return resp
 
 @app.route('/users/<id>')
 def get_user(id):
